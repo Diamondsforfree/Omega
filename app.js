4f8b95d6c5cdbc3617944bf1607e1e1820f36b5a
Diamondsforfree <69599118+Diamondsforfree@users.noreply.github.com>
2020-08-12 23:34:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,3 +20,13 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
+const firebaseConfig = {
+  apiKey: "AIzaSyBCnxBlu-QRTyQ722jtFAN5zIVX_NSDs0Y",
+  authDomain: "facebook-e3dc1.firebaseapp.com",
+  databaseURL: "https://facebook-e3dc1.firebaseio.com",
+  projectId: "facebook-e3dc1",
+  storageBucket: "facebook-e3dc1.appspot.com",
+  messagingSenderId: "1041561918957",
+  appId: "1:1041561918957:web:f86fd450ff7f2d18649be4",
+  measurementId: "G-SNH2542EK3"
+};
