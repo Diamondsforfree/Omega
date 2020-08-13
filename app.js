697eed6f0d3724272c9be91a6fc79274b564ea29
Diamondsforfree <69599118+Diamondsforfree@users.noreply.github.com>
2020-08-13 12:55:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBCnxBlu-QRTyQ722jtFAN5zIVX_NSDs0Y",
+  authDomain: "facebook-e3dc1.firebaseapp.com",
+  databaseURL: "https://facebook-e3dc1.firebaseio.com",
+  projectId: "facebook-e3dc1",
+  storageBucket: "facebook-e3dc1.appspot.com",
+  messagingSenderId: "1041561918957",
+  appId: "1:1041561918957:web:f86fd450ff7f2d18649be4",
+  measurementId: "G-SNH2542EK3"
 };
 
      
